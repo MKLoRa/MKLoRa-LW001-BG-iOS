f54e4400ab04f9844ff6412efbfb92710d8d495b
lovexiaoxia <aadyx2007@163.com>
2021-12-17 09:56:14 +0800

    完善功能
    Signed-off-by: lovexiaoxia <aadyx2007@163.com>

--- MKLoRaWAN-BG/Classes/Functions/SynDataPage/Controller/MKBGSynDataController.m
+++ MKLoRaWAN-BG/Classes/Functions/SynDataPage/Controller/MKBGSynDataController.m
@@ -31,7 +31,7 @@
 #import "MKBGSynTableHeaderView.h"
 #import "MKBGSynDataCell.h"
 
-static NSTimeInterval const parseDataInterval = 0.5;
+static NSTimeInterval const parseDataInterval = 0.1;
 static NSTimeInterval const dataTimerInterval = 30.f;
 
 static NSString *synIconAnimationKey = @"synIconAnimationKey";
@@ -382,14 +382,14 @@ mk_bg_storageDataDelegate>
 
 - (void)parseContentDatas {
     if (self.contentList.count == 0) {
-        if (self.parseTimer) {
-            dispatch_cancel(self.parseTimer);
-        }
         NSInteger number = self.dataList.count;
-        if (ValidStr(self.totalSum) && [self.totalSum integerValue] != number) {
-            number = [self.totalSum integerValue];
-            self.headerView.countLabel.text = [NSString stringWithFormat:@"Count: %ld",(long)number];
+        if (ValidStr(self.totalSum) && [self.totalSum integerValue] == number) {
+            //解析完成
+            if (self.parseTimer) {
+                dispatch_cancel(self.parseTimer);
+            }
         }
+        self.headerView.countLabel.text = [NSString stringWithFormat:@"Count: %ld",(long)number];
         return;
     }
     NSString *firstContent = self.contentList[0];
