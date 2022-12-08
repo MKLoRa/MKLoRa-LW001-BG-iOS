61a5012643ab4378da4f7fc77000fff62f088db0
lovexiaoxia <aadyx2007@163.com>
2022-12-08 14:43:14 +0800

    完善功能

--- MKLoRaWAN-BG/Classes/Functions/TabBarPage/Controller/MKBGTabBarController.m
+++ MKLoRaWAN-BG/Classes/Functions/TabBarPage/Controller/MKBGTabBarController.m
@@ -124,6 +124,9 @@
         [self showAlertWithMsg:@"Reboot successfully!Please reconnect the device." title:@"Dismiss"];
         return;
     }
+    //异常断开
+    NSString *msg = [NSString stringWithFormat:@"Device disconnected for unknown reason.(%@)",type];
+    [self showAlertWithMsg:msg title:@"Dismiss"];
 }
 
 - (void)centralManagerStateChanged{
